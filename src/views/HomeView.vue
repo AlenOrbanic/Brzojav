6600c27f694fd967c5eb1a657e418a81d3618898
AlenOrbanic <aorbanic@student.unipu.hr>
2026-05-07 09:50:41 +0200

    Dodan member count za grupe

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -432,7 +432,18 @@
           <div
             class="contact-info-view d-flex flex-column align-items-center justify-content-center p-5"
           >
-            <AvatarImg :src="contactInfoData.avatar" :size="80" class="mb-3" />
+            <div class="profile-avatar-wrap mb-3">
+              <AvatarImg :src="contactInfoData.avatar" :size="80" :glow="true" />
+              <template v-if="contactInfoData.members?.length">
+                <button class="profile-edit-icon" @click="$refs.groupAvatarInput.click()" data-tooltip="Edit group photo">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                    <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                  </svg>
+                </button>
+                <input type="file" ref="groupAvatarInput" accept="image/*" style="display:none" @change="handleGroupAvatarUpload" />
+              </template>
+            </div>
             <div class="fw-bold fs-5 mb-1 d-flex align-items-center gap-2">
               <template v-if="editingChatName">
                 <input
@@ -617,6 +628,12 @@
                 </template>
                 <template v-else>
                   {{ selectedChat.name }}
+                  <span
+                    v-if="selectedChat.members?.length"
+                    style="font-size: 13px; font-weight: 400; color: gray; margin-left: 4px;"
+                  >
+                    · {{ selectedChat.members.length }} Members
+                  </span>
                 <span
                   class="chat-name-icon-slot"
                   @click="startEditingChatName"
@@ -1354,6 +1371,12 @@ export default {
     },
   },
   methods: {
+    handleGroupAvatarUpload(event) {
+      const file = event.target.files[0];
+      if (!file || !this.contactInfoData) return;
+      this.contactInfoData.avatar = URL.createObjectURL(file);
+      event.target.value = "";
+    },
     kickMember(groupChat, memberIndex) {
       if (!groupChat?.members) return;
       groupChat.members.splice(memberIndex, 1);
